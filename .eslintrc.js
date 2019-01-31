c736a7fefd358a62290b9f98774b1f132fb92e9c
ergotamin <ergotamin.source@gmail.com>
2019-01-31 19:26:11 +0100

    [01/31/19] refresh [e9726a59]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
-  extends: ['airbnb'],
+  parser: "babel-eslint",
+  extends: ["airbnb"],
   parserOptions: {
     ecmaVersion: 6,
     ecmaFeatures: {
@@ -13,21 +13,21 @@ module.exports = {
       es6: true,
       worker: true,
       webextensions: true,
-      'shared-node-browser': true,
-      nashorn: true,
-    },
+      "shared-node-browser": true,
+      nashorn: true
+    }
   },
   rules: {
-    code: '125',
-    'no-undef': 'off',
-    'no-console': 'off',
-    'func-names': 'off',
-    'no-unused-vars': 'off',
-    'no-underscore-dangle': 'off',
-    'prefer-destructuring': 'off',
-    'class-methods-use-this': 'off',
-    'no-useless-constructor': 'off',
-    'import/prefer-default-export': 'off',
-    'import/no-extraneous-dependencies': 'off',
-  },
+    code: "125",
+    "no-undef": "off",
+    "no-console": "off",
+    "func-names": "off",
+    "no-unused-vars": "off",
+    "no-underscore-dangle": "off",
+    "prefer-destructuring": "off",
+    "class-methods-use-this": "off",
+    "no-useless-constructor": "off",
+    "import/prefer-default-export": "off",
+    "import/no-extraneous-dependencies": "off"
+  }
 };
